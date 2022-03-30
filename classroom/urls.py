dac4cf59a5ce77875f1becbaacd2ff94f990c332
Purwa Sadr <purwasadr@gmail.com>
2022-03-30 11:35:19 +0700

    Add materials path

--- classroom/urls.py
+++ classroom/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout_view, name='logout'),
-    path('add-room', views.AddRoom.as_view(), name='add-room')
+    path('add-room', views.AddRoom.as_view(), name='add-room'),
+    path('materials', views.MaterialView.as_view(), name='materials')
 ]
