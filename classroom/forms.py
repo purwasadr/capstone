e332c7ecac571003931824f9c1ceb82b4131092d
Purwa Sadr <purwasadr@gmail.com>
2022-04-02 12:39:28 +0700

    Add AddMaterialForm and AddTaskForm class

--- classroom/forms.py
+++ classroom/forms.py
@@ -17,9 +17,66 @@ class AddRoomForm(forms.Form):
         widget=forms.Textarea(
             attrs={'class': 'form-control', 'rows': '4'}
         ))
-    subject = forms.CharField(max_length=255, required=False, label='subject',
+    subject = forms.CharField(max_length=255, required=False, label='Subject',
         widget=forms.TextInput(
             attrs={'class': 'form-control'}
         ))
     room_place = forms.CharField(max_length=255, required=False, label='Room',
         widget=widget_common())
+
+
+class AddMaterialForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        label='Title',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    description = forms.CharField(
+        max_length=3000, 
+        label='Description',
+        widget=forms.Textarea(
+            attrs={'class': 'form-control', 'rows': '4'}
+        )
+    )
+    files = forms.FileField(
+        widget=forms.ClearableFileInput(
+            attrs={'class': 'form-control', 'multiple': True}
+        ),
+        required=False
+    )
+
+class AddTaskForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        label='Title',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    description = forms.CharField(
+        max_length=3000, 
+        label='Description',
+        widget=forms.Textarea(
+            attrs={'class': 'form-control', 'rows': '4'}
+        )
+    )
+    date = forms.DateTimeField(
+        label='Date',
+        widget=forms.TimeInput(
+            attrs={'class': 'form-control', 'type': 'date'}
+        ),
+    )
+    time = forms.DateTimeField(
+        label='Time',
+        widget=forms.TimeInput(
+            attrs={'class': 'form-control', 'type': 'time'}
+        ),
+    )
+    files = forms.FileField(
+        widget=forms.ClearableFileInput(
+            attrs={'class': 'form-control', 'multiple': True}
+        ),
+        required=False
+    )
