d037b5b9c3bdc6bbefa2dc1d9089d03c2859c6c3
Purwa Sadr <purwasadr@gmail.com>
2022-04-04 10:34:21 +0700

    Fix room_id position in path

--- classroom/urls.py
+++ classroom/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('logout', views.logout_view, name='logout'),
     path('add-room', views.AddRoom.as_view(), name='add-room'),
-    path('materials/<int:room_id>/add-material', views.AddMaterialView.as_view(), name='add-material'),
-    path('materials/<int:room_id>', views.MaterialsView.as_view(), name='materials'),
-    path('tasks/<int:room_id>/add', views.AddTaskView.as_view(), name='add-task'),
-    path('tasks/<int:room_id>', views.TaskView.as_view(), name='tasks')
+    path('materials/file/<int:material_id>', views.material_file, name='material-file'),
+    path('<int:room_id>/materials/add-material', views.AddMaterialView.as_view(), name='add-material'),
+    path('<int:room_id>/materials/<int:material_id>/comments', views.material_comment, name='comments'),
+    path('<int:room_id>/materials', views.MaterialsView.as_view(), name='materials'),
+    path('<int:room_id>/tasks/add', views.AddTaskView.as_view(), name='add-task'),
+    path('<int:room_id>/tasks', views.TaskView.as_view(), name='tasks')
 ]
