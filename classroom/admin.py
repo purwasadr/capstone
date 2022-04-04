ba6ed9c85be0ef936d6eac7e3c1785f1d6d80354
Purwa Sadr <purwasadr@gmail.com>
2022-04-04 10:29:23 +0700

    Add register MaterialComment

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from classroom.models import Material, MaterialFile, Room, User
+from classroom.models import Material, MaterialComment, MaterialFile, Room, User
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Material)
 admin.site.register(MaterialFile)
+admin.site.register(MaterialComment)
