48a5ea28546ad95d759341c448ccc84836810680
Purwa Sadr <purwasadr@gmail.com>
2022-04-06 16:20:54 +0700

    Register Task and TaskFile model

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from classroom.models import Material, MaterialComment, MaterialFile, Room, User
+from classroom.models import Material, MaterialComment, MaterialFile, Room, Task, TaskFile, User
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +8,5 @@ admin.site.register(Room)
 admin.site.register(Material)
 admin.site.register(MaterialFile)
 admin.site.register(MaterialComment)
+admin.site.register(Task)
+admin.site.register(TaskFile)
