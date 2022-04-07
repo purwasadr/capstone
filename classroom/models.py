3dba198218a65dfcd75e8e1ea12d55f93da06e21
Purwa Sadr <purwasadr@gmail.com>
2022-04-07 14:49:59 +0700

    Add members field in Room Model

--- classroom/models.py
+++ classroom/models.py
@@ -32,6 +32,7 @@ class Room(models.Model):
     room_place = models.CharField(max_length=255, blank=True)
     room_code = models.CharField(max_length=12)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms')
+    members = models.ManyToManyField(User, blank=True, related_name='room_members')
 
     def __str__(self):
         return self.name
