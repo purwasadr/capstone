459fbe4c89f71b328aacf8559a968e6e490ae731
Purwa Sadr <purwasadr@gmail.com>
2022-04-13 06:52:50 +0700

    Change function based to class based views, Add GeneralView, AuthView, and ReturnTaskView and remove unused code

--- classroom/views.py
+++ classroom/views.py
@@ -5,7 +5,7 @@ from django.db import IntegrityError
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import FileResponse, HttpResponseRedirect, JsonResponse
+from django.http import FileResponse, HttpResponseForbidden, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import View
@@ -24,13 +24,24 @@ def generate_clas_code():
     random.seed = (os.urandom(1024))
     return ''.join(random.choice(chars) for i in range(length))
 
-def login_view(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('index'))
+class AuthView(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('index')
+        return super().dispatch(request, *args, **kwargs)
 
-    if request.method == 'POST':
+class GeneralView(View):
 
-        # Attempt to sign user in
+    @method_decorator(login_required(redirect_field_name=''))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+class LoginView(AuthView):
+    def get(self, request):
+        return render(request, "classroom/login.html")
+
+    def post(self, request):
+         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
@@ -42,28 +53,27 @@ def login_view(request):
         else:
             messages.error(request, 'Invalid username and/or password.')
             return render(request, 'classroom/login.html')
-    else:
-        return render(request, "classroom/login.html")
+    
+class LogoutView(GeneralView):
 
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse("index"))
 
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+class RegisterView(AuthView):
+    def get(self, request):
+        return render(request, 'classroom/register.html')
 
-
-def register(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('index'))
-
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
 
         # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
+        password = request.POST['password']
+        confirmation = request.POST['confirmation']
+
         if password != confirmation:
-            return render(request, "classroom/register.html", {
+            return render(request, 'classroom/register.html', {
                 "message": "Passwords must match."
             })
 
@@ -72,29 +82,30 @@ def register(request):
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "classroom/register.html", {
-                "message": "Username already taken."
+            return render(request, 'classroom/register.html', {
+                'message': 'Username already taken.'
             })
+
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, 'classroom/register.html')
+        return redirect('index')
 
-@login_required()
-def index(request):
-    # clases = Clas.objects.filter(Q(author__id=request.user.id) | Q(members__id=request.user.id)).all()
-    clases = request.user.clas_members.all().union(request.user.clases.all())
-    # clases = Clas.objects.filter(Q(author__id=request.user.id)).all()
-    return render(request, 'classroom/index.html', {
-        'clases': clases
-    })
 
-@method_decorator(login_required, name='dispatch')
-class AddClas(View):
+class IndexView(GeneralView):
+    def get(self, request):
+        # clases = Clas.objects.filter(Q(author__id=request.user.id) | Q(members__id=request.user.id)).all()
+        clases = request.user.clases_owned_and_joined()
+        # clases = request.user.clas_members.all().union(request.user.clases.all())
+        # clases = Clas.objects.filter(Q(author__id=request.user.id)).all()
+        return render(request, 'classroom/index.html', {
+            'clases': clases
+        })
+
+
+class AddClas(GeneralView):
     template_path = 'classroom/add-clas.html'
 
     def get(self, request):
-        return render(request, self.template_path, {'form': AddClasForm})
+        return render(request, self.template_path, { 'form': AddClasForm })
     
     def post(self, request):
         form = AddClasForm(request.POST)
@@ -102,28 +113,65 @@ class AddClas(View):
         if form.is_valid():
             name = form.cleaned_data['name']
             description = form.cleaned_data['description']
+            section = form.cleaned_data['section']
             subject = form.cleaned_data['subject']
             room = form.cleaned_data['room']
             clas_code = generate_clas_code()
             Clas.objects.create(
                 name = name,
                 description = description,
+                section = section,
                 subject = subject,
                 room = room,
                 clas_code = clas_code,
                 author = request.user
             )
             return HttpResponseRedirect(reverse('index'))
-@method_decorator(login_required, name='dispatch')
-class MaterialsView(View):
+
+class EditClasView(GeneralView):
+    def post(self, request, clas_id):
+        clas = get_object_or_404(Clas, pk=clas_id)
+
+        form = AddClasForm(request.POST)
+        if form.is_valid():
+            clas.name = form.cleaned_data['name']
+            clas.description = form.cleaned_data['description']
+            clas.section = form.cleaned_data['section']
+            clas.subject = form.cleaned_data['subject']
+            clas.room = form.cleaned_data['room']
+            clas.save()
+            return redirect('materials', clas_id)
+
+        return redirect('materials', clas_id)
+
+class DeleteClasView(GeneralView):
+    def post(self, request, clas_id):
+        clas = get_object_or_404(Clas, pk=clas_id)
+        if request.user != clas.author:
+            return HttpResponseForbidden()
+
+        clas.delete()
+        return redirect('index')
+
+class MaterialsView(GeneralView):
     template_path = 'classroom/materials.html'
     def get(self, request, clas_id):
         clas = get_object_or_404(Clas, pk=clas_id)
+        form = AddClasForm(initial={ 
+            'name': clas.name, 
+            'description': clas.description,
+            'section': clas.section,
+            'subject': clas.subject, 
+            'room': clas.room
+            }
+        )
+
         materials = Material.objects.filter(clas=clas_id).order_by('-created_at').all()
         return render(request, self.template_path, {
             'clas': clas,
             'materials': materials,
             'page': 'materials',
+            'form': form,
             'breadcrumb': context_breadcrumb(request.path, clas)
         })
     
@@ -142,8 +190,7 @@ def context_breadcrumb(request_path: str, clas: Clas):
     return breadcrumbs
 
 
-@method_decorator(login_required, name='dispatch')
-class AddMaterialView(View):
+class AddMaterialView(GeneralView):
     template_name = 'classroom/add-material.html'
 
     def get(self, request, clas_id):
@@ -166,7 +213,6 @@ class AddMaterialView(View):
                 MaterialFile.objects.create(filename=file.name, file=file, material=material)
             return HttpResponseRedirect(reverse('materials', args=(clas_id,)))
 
-
 def material_file(request, material_id):
     if not request.user.is_authenticated:
         return
@@ -176,6 +222,15 @@ def material_file(request, material_id):
 
     return FileResponse(file, as_attachment=True, filename=material.filename)
 
+def task_file(request, task_id):
+    if not request.user.is_authenticated:
+        return
+
+    task = TaskFile.objects.get(pk=task_id)
+    file = task.file
+
+    return FileResponse(file, as_attachment=True, filename=task.filename)
+
 def material_comment(request, clas_id, material_id):
     if not request.user.is_authenticated:
         return JsonResponse({'error': 'You must authenticated'}, status=401)
@@ -183,7 +238,7 @@ def material_comment(request, clas_id, material_id):
     try:
         material = Material.objects.get(pk=material_id)
     except Material.DoesNotExist:
-        return JsonResponse({"error": "Material not found."}, status=404)
+        return JsonResponse({'error': 'Material not found.'}, status=404)
 
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -207,7 +262,8 @@ def material_comment(request, clas_id, material_id):
 
         return JsonResponse({
                 'success': 'Success add comment',
-                'data': comments
+                'data': comments,
+                'count': material.comments.count()
             },
             status=201
         )
@@ -229,29 +285,29 @@ def material_comment(request, clas_id, material_id):
 
     return JsonResponse({
                 'success': 'Success',
-                'data': comments_json
+                'data': comments_json,
+                'count': material.comments.count()
             },
             status=201
         )
 
-    
-class TaskView(View):
+class TaskView(GeneralView):
     template_name = 'classroom/tasks.html'
 
     def get(self, request, clas_id):
         tasks = Task.objects.filter(clas=clas_id).order_by('-created_at').all()
+        clas = get_object_or_404(Clas, pk=clas_id)
        
         return render(request, self.template_name, {
-            'clas_id': clas_id,
+            'clas': clas,
             'tasks': tasks,
             'page': 'tasks',
         })
 
-class AddTaskView(View):
+class AddTaskView(GeneralView):
     template_name = 'classroom/add-task.html'
 
     def get(self, request, clas_id):
-        
         return render(request, self.template_name, {
             'clas_id': clas_id,
             'form': AddTaskForm(),
@@ -259,7 +315,6 @@ class AddTaskView(View):
 
     def post(self, request, clas_id):
         form = AddTaskForm(request.POST)
-        
         files = request.FILES.getlist('files')
         
         if form.is_valid():
@@ -284,7 +339,7 @@ class AddTaskView(View):
                 TaskFile.objects.create(filename=file.name, file=file, task=task)
             return HttpResponseRedirect(reverse('tasks', args=(clas_id,)))
 
-class TaskDetailView(View):
+class TaskDetailView(GeneralView):
     template_name = 'classroom/task-detail.html'
 
     def get(self, request, clas_id, task_id):
@@ -302,7 +357,7 @@ class TaskDetailView(View):
             'page_name': 'instruction'
         })
 
-class AddTaskSubmitView(View):
+class AddTaskSubmitView(GeneralView):
     def post(self, request, clas_id, task_id):
         task = Task.objects.get(pk=task_id)
         files = request.FILES.getlist('work-files')
@@ -326,27 +381,14 @@ def join_clas(request):
     search_clas_code = Clas.objects.filter(clas_code=clas_code).first()
 
     if search_clas_code == None:
-        return JsonResponse({
-                'error': 'Code not match any class code',
-            },
-            status=404
-        )
+        return JsonResponse({ 'error': 'Code not match any class code' }, status=404)
     if search_clas_code.members.filter(pk=request.user.id):
-        return JsonResponse({
-                'error': 'You already join',
-            },
-            status=404
-        )
+        return JsonResponse({ 'error': 'You already join' }, status=404)
 
     if search_clas_code.author.id == request.user.id:
-        return JsonResponse({
-                'error': 'You author this class',
-            },
-            status=404
-        )
+        return JsonResponse({ 'error': 'You author this class' }, status=404)
 
     search_clas_code.members.add(request.user)
-    
     return JsonResponse({
                 'success': 'Success',
                 'data': {
@@ -356,7 +398,7 @@ def join_clas(request):
             status=201
         )
 
-class AddTaskFileView(View):
+class AddTaskFileView(GeneralView):
     def post(self, request, clas_id, task_id):
         files = request.FILES.getlist('work-files')
         task = Task.objects.get(pk=task_id)
@@ -365,44 +407,41 @@ class AddTaskFileView(View):
             TaskSubmitFile.objects.create(filename=file.name, file=file, task=task, uploader=request.user)
         return HttpResponseRedirect(reverse('task-detail', args=(clas_id, task_id)))
 
-class ChangeTaskFileView(View):
+class ChangeTaskFileView(GeneralView):
     def post(self, request, clas_id, task_id):
         files = request.FILES.getlist('work-files')
         task = Task.objects.get(pk=task_id)
 
-        task.submitted_files.all().delete()
+        task.submitted_files.filter(uploader=request.user).delete()
 
         for file in files:
             TaskSubmitFile.objects.create(filename=file.name, file=file, task=task, uploader=request.user)
         return HttpResponseRedirect(reverse('task-detail', args=(clas_id, task_id)))
 
-class DeleteTaskFileView(View):
+class DeleteTaskFileView(GeneralView):
     def post(self, request, clas_id, task_id):
         task = Task.objects.get(pk=task_id)
 
-        task.submitted_files.all().delete()
+        task.submitted_files.filter(uploader=request.user).delete()
 
         return HttpResponseRedirect(reverse('task-detail', args=(clas_id, task_id)))
 
-class SubmitTaskView(View):
+class SubmitTaskView(GeneralView):
     def post(self, request, clas_id, task_id):
         task = Task.objects.get(pk=task_id)
 
         task.users_submitted.add(request.user)
 
-        # return HttpResponseRedirect(reverse('task-detail', args=(clas_id, task_id)))
-        return redirect('task-detail', clas_id, task_id, permanent=True)
+        return redirect('task-detail', clas_id, task_id)
 
-class UnSubmitTaskView(View):
+class UnSubmitTaskView(GeneralView):
     def post(self, request, clas_id, task_id):
         task = Task.objects.get(pk=task_id)
 
         task.users_submitted.remove(request.user)
-
-        # return HttpResponseRedirect(reverse('task-detail', args=(clas_id, task_id)))
         return redirect('task-detail', clas_id, task_id, permanent=True)
 
-class TaskSubmission(View):
+class TaskSubmission(GeneralView):
     template_name = 'classroom/task-submission.html'
 
     def get(self, request, clas_id, task_id):
@@ -414,7 +453,7 @@ class TaskSubmission(View):
             'task': task,
             'page_name': 'submission',
             'users_submitted': user_submitted,
-            'users_assigned': User.objects.exclude(Q(submitted_tasks=task_id) | Q(clases=task.clas.id)).all()
+            'users_assigned': User.objects.exclude(Q(submitted_tasks=task) | Q(clases=task.clas)).filter(clas_members=clas).all()
         })
         # Q(author__id=request.user.id) | Q(members__id=request.user.id)
 
@@ -423,7 +462,24 @@ class TaskSubmissionDetail(View):
     def get(self, request, clas_id, task_id, user_id):
         task = Task.objects.get(pk=task_id)
         submitted_files = task.submitted_files.filter(uploader=user_id)
+        user_submission = task.clas.members.filter(pk=user_id).first()
+        task_user_is_returned = task.user_task_returned.filter(pk=user_id).exists()
+        is_user_submit = task.users_submitted.filter(pk=user_id).exists()
         return render(request, self.template_name, {
+            'clas_id': clas_id,
+            'task_id': task_id,
+            'user_id': user_id,
             'task': task,
-            'submitted_files': submitted_files
+            'submitted_files': submitted_files,
+            'task_user_is_returned': task_user_is_returned,
+            'user_submission': user_submission,
+            'is_user_submit': is_user_submit
         })
+
+class ReturnTaskView(View):
+    def post(self, request, clas_id, task_id, user_id):
+        task = Task.objects.get(pk=task_id)
+        user = User.objects.get(pk=user_id)
+        task.user_task_returned.add(user)
+
+        return redirect('task-submission-detail', clas_id, task_id, user_id)
